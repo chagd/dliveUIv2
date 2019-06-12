1c0f0467c048759a732bf38179f1d836f4a308e4
chagarlamudi dharun <35214072+chagd@users.noreply.github.com>
2019-06-12 16:37:06 +0530

    added gps following; remapped topics

--- dliveui/scripts/get_point.py
+++ dliveui/scripts/get_point.py
@@ -12,10 +12,11 @@ def callback(msg):
     x = float(msg.point.x) + 714244.59
     y = float(msg.point.y) + 3159474.69
     point = utm.to_latlon(x, y, 43, 'R')
-    print point
+    print "destination set to " + str(point[0]) + ", " + str(point[1])
     print ""
 
 rospy.init_node('set_goal_rviz', anonymous=True)
+print ""
 
 rospy.Subscriber('clicked_point', PointStamped, callback = callback)
 
